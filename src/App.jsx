b8746819427a171f72a77b781099fc2dda214e0b
Derradji-gns <derradjiamineabdelbasset@gmail.com>
2025-03-28 02:07:55 +0100

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Projects from './projects'
+
+function App() {
+  
+
+  return (
+    <div className="bg-gray-900">
+      <Projects/>
+      
+    </div>
+  )
+}
+
+export default App
